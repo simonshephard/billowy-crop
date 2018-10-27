7b3ee6ebf7b5e016604040392fb00b49a1ba3a56
Glitch (billowy-crop) <none>
2018-10-27 23:01:14 +0000

    🐵🏋 Checkpoint ./routes/api.js:1715671/479

--- routes/api.js
+++ routes/api.js
@@ -21,22 +21,30 @@ module.exports = function (app) {
 
     .get(function (req, res){
       var project = req.params.project;
+    
+      // get data for project
+      // db.connection not mongoose - check other recet projects
+    
 
     })
 
     .post(function (req, res){
       var project = req.params.project;
 
+      // get data from project post object
+      // create and save db data
+
+    
     })
 
     .put(function (req, res){
       var project = req.params.project;
-
+      // amend project data and save
     })
 
     .delete(function (req, res){
       var project = req.params.project;
-
+      // delete project data
     });
 
 };
