8827d9e083a9b5eaae7de03bad394d8ee17de334
Glitch (billowy-crop) <none>
2018-10-28 14:35:26 +0000

    👶🐜 Checkpoint ./routes/api.js:1715671/1417

--- routes/api.js
+++ routes/api.js
@@ -20,23 +20,29 @@ module.exports = function (app) {
 
   app.route('/api/issues/:project')
 
+    // I can GET /api/issues/{projectname} for an array of all issues on that specific project with all the information for each issue as was returned when posted.
+    // I can filter my get request by also passing along any field and value in the query(ie. /api/issues/{project}?open=false). I can pass along as many fields/values as I want.
     .get(function (req, res){
       // THIS WORKS WITHOUT DB
       // res.json({result: "ok"});
+    
       // THIS ALSO WORKS WITHOUT DB
       // res.json({result: "ok", project: req.params.project});
     
-    // I can GET /api/issues/{projectname} for an array of all issues on that specific project with all the information for each issue as was returned when posted.
-    // I can filter my get request by also passing along any field and value in the query(ie. /api/issues/{project}?open=false). I can pass along as many fields/values as I want.
-      var project = req.params.project;
-      var filter = req.query || {};
-      filter.issue_title = project;
-      db.collection('projects')
-        .find(filter)
-        .toArray()
-        .then((docs) => {
-          res.json(docs)
-        })
+      // THIS ALSO WORKS WITHOUT DB
+      // *https://billowy-crop.glitch.me/api/issues/newproj?open=false&new_prop=new_prop
+      // *{"result": "ok", "filter": {"open": "false", "new_prop": "new_prop", "issue_title": "newproj"}}
+      // var project = req.params.project;
+      // var filter = req.query || {};
+      // filter.issue_title = project;
+      // res.json({result: "ok", filter: filter});
+    
+      // db.collection('projects')
+      //   .find(filter)
+      //   .toArray()
+      //   .then((docs) => {
+      //     res.json(docs)
+      //   })
     })
 
     .post(function (req, res){
