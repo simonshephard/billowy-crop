ff7b798137db8a4ac08a48c7889a668faacffc2b
Glitch (billowy-crop) <none>
2018-10-28 14:15:17 +0000

    🐪🚒 Checkpoint ./routes/api.js:1715671/672

--- routes/api.js
+++ routes/api.js
@@ -21,17 +21,19 @@ MongoClient.connect(CONNECTION_STRING, function(err, db) {
   app.route('/api/issues/:project')
 
     .get(function (req, res){
+      res.json({result: "ok"});
+    
     // I can GET /api/issues/{projectname} for an array of all issues on that specific project with all the information for each issue as was returned when posted.
     // I can filter my get request by also passing along any field and value in the query(ie. /api/issues/{project}?open=false). I can pass along as many fields/values as I want.
-      var project = req.params.project;
-      var filter = req.query || {};
-      filter.issue_title = project;
-      db.collection('projects')
-        .find(filter)
-        .toArray()
-        .then((docs) => {
-          res.json(docs)
-        })
+      // var project = req.params.project;
+      // var filter = req.query || {};
+      // filter.issue_title = project;
+      // db.collection('projects')
+      //   .find(filter)
+      //   .toArray()
+      //   .then((docs) => {
+      //     res.json(docs)
+      //   })
     })
 
     .post(function (req, res){
