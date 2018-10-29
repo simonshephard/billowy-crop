685c6e1728bcc9d2f56fc23576beb07dff7ed0cc
Glitch (billowy-crop) <none>
2018-10-29 18:50:23 +0000

    🌲💷 Checkpoint ./tests/2_functional-tests.js:1715671/2585

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -138,7 +138,34 @@ suite('Functional Tests', function() {
       });
       
       test('Multiple fields to update', function(done) {
-        
+       chai.request(server)
+        .post('/api/issues/test')
+        .send({
+          issue_title: 'Title',
+          issue_text: 'text',
+          created_by: 'Functional Test - Every field filled in',
+          assigned_to: 'Chai and Mocha',
+          status_text: 'In QA'
+        })
+        .end(function(err, res){
+         chai.request(server)
+          .put('/api/issues/test')
+          .send({
+            "_id": ObjectId(res.insertedId),
+            issue_title: 'Title2',
+            issue_text: 'Text2',
+            created_by: 'Created2',
+          })
+          .end(function(err2, res2){
+            assert.equal(res2.status, 200);
+            assert.equal(res2.body.issue_title, 'Title2');
+            assert.equal(res2.body.issue_text, 'Text2');
+            assert.equal(res2.body.created_by, 'Created2');
+            done();
+           });
+         done();
+        });
+
       });
       
     });
@@ -166,11 +193,45 @@ suite('Functional Tests', function() {
       });
       
       test('One filter', function(done) {
+        chai.request(server)
+        .get('/api/issues/test?open=true')
+        .query({})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.isArray(res.body);
+          assert.property(res.body[0], 'issue_title');
+          assert.property(res.body[0], 'issue_text');
+          assert.property(res.body[0], 'created_on');
+          assert.property(res.body[0], 'updated_on');
+          assert.property(res.body[0], 'created_by');
+          assert.property(res.body[0], 'assigned_to');
+          assert.property(res.body[0], 'open');
+          assert.property(res.body[0], 'status_text');
+          assert.property(res.body[0], '_id');
+          done();
+        });
         
       });
       
       test('Multiple filters (test for multiple fields you know will be in the db for a return)', function(done) {
-        
+        chai.request(server)
+        .get('/api/issues/test?open=true&issue_title=Title')
+        .query({})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.isArray(res.body);
+          assert.property(res.body[0], 'issue_title');
+          assert.property(res.body[0], 'issue_text');
+          assert.property(res.body[0], 'created_on');
+          assert.property(res.body[0], 'updated_on');
+          assert.property(res.body[0], 'created_by');
+          assert.property(res.body[0], 'assigned_to');
+          assert.property(res.body[0], 'open');
+          assert.property(res.body[0], 'status_text');
+          assert.property(res.body[0], '_id');
+          done();
+        });
+
       });
       
     });
