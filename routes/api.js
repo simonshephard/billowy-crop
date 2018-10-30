01ecd281a1c869c7e7bc8091486c39e9b35b28e3
Glitch (billowy-crop) <none>
2018-10-30 22:09:44 +0000

    📮🐺 Checkpoint ./routes/api.js:1715671/491

--- routes/api.js
+++ routes/api.js
@@ -39,7 +39,7 @@ module.exports = function (app) {
 
     var project = req.params.project;
     if (!req.body.issue_title || !req.body.issue_text || !req.body.created_by) {res.json({result: 'required not completed'});}
-    var new_entry = {}; // NOTE - do not copy req.body as creates a pointer not a copy
+    var new_entry = {};
     new_entry.issue_title = req.body.issue_title;
     new_entry.issue_text = req.body.issue_text;
     new_entry.created_by = req.body.created_by;      
@@ -52,10 +52,8 @@ module.exports = function (app) {
     MongoClient.connect(CONNECTION_STRING, function(err, db) {
       db.collection(project)
       .insertOne(new_entry, (err, doc) => {
-        //console.log({_id: doc.insertedId});
         new_entry._id = doc.insertedId
         const results = [new_entry];
-        //console.log({docs: results});
         res.json({docs: results});
       });
     });
@@ -66,7 +64,7 @@ module.exports = function (app) {
 
     var project = req.params.project;
     var id = req.body._id;
-    var updated_entry = {}; // NOTE - do not copy req.body as creates a pointer not a copy
+    var updated_entry = {};
     updated_entry.issue_title = req.body.issue_title || '';
     updated_entry.issue_text = req.body.issue_text || '';
     updated_entry.created_by = req.body.created_by || '';      
@@ -78,7 +76,8 @@ module.exports = function (app) {
 
     MongoClient.connect(CONNECTION_STRING, function(err, db) {
       db.collection(project)
-      .findAndModify({_id: ObjectId(id)},[['_id',1]], {$set: updated_entry}, {new: true}, (err,doc) => {
+      // .findAndModify({_id: ObjectId(id)},[['_id',1]], {$set: updated_entry}, {new: true}, (err,doc) => {
+      .findAndModify({_id: ObjectId(id)}, {$set: updated_entry}, {new: true}, (err,doc) => {
         if (err) {
           console.error(err);
           res.json({update_err: err, result: 'could not update ' + id});
@@ -96,20 +95,25 @@ module.exports = function (app) {
   .delete(function (req, res){
 
     var project = req.params.project;
-    // console.log({reqBody: req.body});
     var id = req.body._id;
-    if (id === '') {res.json({result: 'no _id sent - _id error'});}
+    if (id === '') {
+      res.json({result: 'no _id sent - _id error'});
+      return;
+    }
 
     MongoClient.connect(CONNECTION_STRING, function(err, db) {
       db.collection(project)
-      .deleteOne({_id: ObjectId(id)}, (err, doc) => {
+      .deleteOne({_id: id}, (err, doc) => {
         if (err) {
           console.error(err);
           res.json({delete_err: err, result: 'failed: could not delete ' + id});
+          done();
         } else if (doc) {
           res.json({result: 'success: deleted ' + id});
+          return;
         } else {
           res.json({other: 'no error, no delete', result: 'failed: could not delete ' + id});
+          return;
         }
       });
     });
